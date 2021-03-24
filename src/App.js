b6a584b5f76c26035ad07dc4422bf74a6fbeac31
Rikhil <rikhilshah@hotmail.co.uk>
2021-03-24 17:46:43 +0000

    create user temp added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 // Imports from libraries
 import 'bootstrap/dist/css/bootstrap.min.css' // Bootstrap css
-import {Container} from 'react-bootstrap' // Container for all Rows/Components
+import {Container, Button} from 'react-bootstrap' // Container for all Rows/Components
+import {useState} from 'react'
 
 import Login from './components/Login'
-//import Form from './components/Form'
+import CreateUser from './components/CreateUser'
+
+
+
 
 function App() {
+  const [screen, setScreen] = useState("login")
+
+  function createUser()
+  {
+    setScreen("createUser")
+  }
+
+
   return (
     <div className="App">
       <Container className="mt-5">
-        <Login />
+        {screen === "login" && (
+          <div>
+            <Login />
+            <Button className="mt-5" onClick={createUser}> Create User </Button>
+          </div>
+          )}
+
+        {screen === "createUser" && (
+          <CreateUser />
+        )}
+
+
+
       </Container>
     </div>
   );
