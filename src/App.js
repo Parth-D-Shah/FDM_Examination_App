efa65c0ec869618020b84ee32d8d721870c76cf2
Rikhil <rikhilshah@hotmail.co.uk>
2021-03-24 16:45:28 +0000

    temp changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import {Container} from 'react-bootstrap' // Container for all Rows/Components
 import {useState, useEffect} from 'react'; // React states to store API info
 
 import Login from './components/Login'
+//import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <Container className="">
+      <Container className="mt-5">
         <Login />
       </Container>
     </div>
