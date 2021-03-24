804fa91c41d23378395eeae82c3405fb9407bcd6
Rikhil <rikhilshah@hotmail.co.uk>
2021-03-24 17:36:52 +0000

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // Imports from libraries
 import 'bootstrap/dist/css/bootstrap.min.css' // Bootstrap css
 import {Container} from 'react-bootstrap' // Container for all Rows/Components
-import {useState, useEffect} from 'react'; // React states to store API info
 
 import Login from './components/Login'
 //import Form from './components/Form'
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css' // Bootstrap css
-import {Row, Col, Card, Form, Button} from 'react-bootstrap' // Container for all Rows/Components
-import {useState, useEffect} from 'react'; // React states to store API info
+import {Form, Button} from 'react-bootstrap' // Container for all Rows/Components
+import {useState} from 'react'; // React states to store API info
 
 const Login = () => {
 
@@ -32,24 +32,24 @@ const Login = () => {
     
 
     return (
-    <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} required/>
-            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} required/>
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" name="pwd" onChange={handleChange} required/>
-        </Form.Group>
-        
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-    </Form>
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" name="pwd" onChange={handleChange} required/>
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     
     )
 }
