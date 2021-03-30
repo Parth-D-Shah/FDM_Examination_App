1eea69707607bf96c6ca04bf2b6ed658bad58f70
Rikhil <rikhil.shah@se19.qmul.ac.uk>
2021-03-30 18:00:37 +0100

    progress with dashboard

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,26 +1,116 @@
 import 'bootstrap/dist/css/bootstrap.min.css' // Bootstrap css
 import './Dashboard.css';
-import {Container, Navbar, Nav, Button} from 'react-bootstrap' // Container for all Rows/Components
+import {Container, Navbar, Nav, Button, Row, Col, Card} from 'react-bootstrap'; // Container for all Rows/Components
+
+import logo from '../assets/logo-blue.png'
+import iconYourAccount from '../assets/your-account-blue.png';
+import iconManageUsers from '../assets/manage-users-blue.png';
+import iconTicket from '../assets/ticket-blue.png';
+
+
 
 const Dashboard = () => {
+
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand className="brand" href="">Online Examination Application</Navbar.Brand>
+                    <Navbar.Brand className="brand">
+                        <Row className="align-items-center">
+                            <Col className="">
+                                <img className="logo img-fluid" src={logo} alt="Logo"/>
+                            </Col>
+                            <Col className="p-0">
+                                Online Examination Application
+                            </Col>
+                        </Row>
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="links ml-auto ">
-                            <Nav.Link className="mr-4" href="">...</Nav.Link>
-                            <Nav.Link className="mr-4" href="">...</Nav.Link>
-                            <Nav.Link className="mr-4" href="">...</Nav.Link>
-                            <Button variant="outline-light">Sign Out</Button>
+                            <Nav.Link className="mr-3" href="">Dashboard</Nav.Link>
+                            <Nav.Link className="mr-3" href="">Your Account</Nav.Link>
+                            <Nav.Link className="mr-3" href="">Manage Users</Nav.Link>
+                            <Nav.Link className="mr-3" href="">Manage Tickets</Nav.Link>
                         </Nav>
+                        <Button className="outlineButton" variant="outline-primary">Sign Out</Button>
                     </Navbar.Collapse>
                 
                 </Container>
             </Navbar>
+
+            <Container>
+
+                <Row className="mt-5 pb-3">
+                    <Col className="">
+                        <div className="welcomeMsg float-left">
+                            Welcome, Rikhil Shah
+                        </div>
+
+                        <div className="userDetails float-right">
+                            <p className="m-0"> <strong>ID:</strong> 00001 </p>
+                            <p className="m-0"> <strong>User: </strong> System Admin </p>
+                            <p className="m-0"> <strong>Email: </strong> ec19148@qmul.ac.uk </p>
+                        </div>
+                    </Col>
+                </Row>
+                
+                <Row className="mt-5">
+                    <Col className="d-flex justify-content-center">
+                        <Card bg="dark" text="white" className="card text-center">
+                            <Card.Img className="pt-3 cardImage mx-auto" variant="top" src={iconYourAccount}/>
+                            <Card.Body>
+                                <Card.Title> Your Account </Card.Title>
+                                <Card.Text>
+                                    View information about your account and change details.
+                                </Card.Text>
+                                <div className="d-flex justify-content-center">
+                                    <Button className="normalButton" variant="primary">Your Account</Button>
+                                </div>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                    <Col className="d-flex justify-content-center">
+                        <Card bg="dark" text="white" className="card text-center">
+                            <Card.Img className="pt-3 cardImage mx-auto" variant="top" src={iconManageUsers}/>
+                            <Card.Body>
+                                <Card.Title> Manage System Users </Card.Title>
+                                <Card.Text>
+                                    Create and manage user accounts for the system.
+                                </Card.Text>
+                                <div className="d-flex justify-content-center">
+                                    <Button className="normalButton" variant="primary">Manage Users</Button>
+                                </div>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col className="d-flex justify-content-center">
+                        <Card bg="dark" text="white" className="card text-center">
+                            <Card.Img className="pt-3 cardImage mx-auto" variant="top" src={iconTicket}/>
+                            <Card.Body>
+                                <Card.Title> Manage Support Tickets </Card.Title>
+                                <Card.Text>
+                                    View and resolve support tickets submitted by users.
+                                </Card.Text>
+                                <div className="d-flex justify-content-center">
+                                    <Button className="normalButton" variant="primary">Manage Tickets</Button>
+                                </div>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                
+
+
+
+            </Container>
         </div>
     )
 }
