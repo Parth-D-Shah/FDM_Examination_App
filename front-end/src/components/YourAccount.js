9b55aa28dbb18e6c1422c63eb95b86667e8d96f5
ParthShah101 <>
2021-03-31 23:30:06 +0100

    your account was added

--- front-end/src/components/YourAccount.js
+++ front-end/src/components/YourAccount.js
@@ -1,58 +1,58 @@
 import 'bootstrap/dist/css/bootstrap.min.css' // Bootstrap css
+import './YourAccount.css'
 import {Form, Container, Button, ButtonGroup} from 'react-bootstrap'; // Container for all Rows/Components
-
+import Swal from 'sweetalert2'
 import {useState} from 'react'; // React states to store API info
 const Dashboard = () => {
-    const [oldpassword, setOldPassword] = useState("")
-    const [password, setNewPassword] = useState("")
-
-
     function handleSubmit(event)
     {
         event.preventDefault(event);
-        console.log(oldpassword, password)
+        /*console.log(email, password)*/
     }
 
-    function handleChange(event)
+    async function handleEmailChange()
     {
-        var changedBox = event.target.name
-        var value = event.target.value
+        const {value: email} = await Swal.fire
+        ({
+            title: 'New Email Address',
+            input: 'email',
+            confirmButtonText: 'Submit',
+            showCancelButton: true,
+        })
 
-        if (changedBox === "oldpassword")
-        {
-            setOldPassword(value)
+        if (email) {
+            Swal.fire(`Entered email: ${email}`)
         }
+    }
 
-        else if (changedBox === "pwd")
-        {
-            setNewPassword(value)
-        }
+    async function handlePasswordChange()
+    {
+        const {value: password} = await Swal.fire
+        ({
+            title: 'New Password',
+            input: 'password',
+            confirmButtonText: 'Submit',
+            showCancelButton: true,
+        })
     }
 
     return (
         <div>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label id="label">Current Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter Current Password" name="oldpassword" onChange={handleChange} required/>
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label id="label">New Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter New Password" name="pwd" onChange={handleChange} required/>
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label id="label">Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm New Password" name="pwd" onChange={handleChange} required/>
-                </Form.Group>
-                
-                <ButtonGroup className="btnGroup">
-                    <Button className="btnSubmit" variant="primary" type="submit">
-                        Confirm
-                    </Button>
-                </ButtonGroup>
-            </Form>
+            <div id='textFormatting'>
+                Name: Rikhil Shah
+                <br/>
+                ID: 00001
+                <br/>
+                Email: ec19148@qmul.ac.uk &nbsp;
+                <Button variant="outline-primary" onClick={handleEmailChange}>
+                    Change
+                </Button>
+                <br/>
+                Pasword: ****************** &nbsp; &nbsp;
+                <Button variant="outline-primary" onClick={handlePasswordChange}>
+                    Change
+                </Button>
+            </div>
         </div>
     )
     }
