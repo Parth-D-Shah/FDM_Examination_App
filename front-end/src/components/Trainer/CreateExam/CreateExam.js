18329830bc874a2f7d4bb3ea85a9bd44c6b82584
ParthShah101 <ParthShah2065@outlook.com>
2021-04-07 19:52:17 +0100

    changes

--- front-end/src/components/Trainer/CreateExam/CreateExam.js
+++ front-end/src/components/Trainer/CreateExam/CreateExam.js
@@ -15,28 +15,29 @@ const Dashboard = ({loggedInUser}) => {
             <div>
             <Form >
                 <Form.Group controlId="formBasicQuestion">
-                    <Form.Label id="label">Question</Form.Label>
-                    <Form.Control type="text" placeholder="Enter question" name="Question"/>
+                    <Form.Label id="label">Question: </Form.Label>
+                    <Form.Control type="textarea" placeholder="Enter question" name="Question"/>
                 </Form.Group>
 
                 <Form.Row>
-                            <Form.Group as={Col}>
-                                <Form.Label>Answer</Form.Label>
-                                <Form.Control name="editFname" type="text" placeholder="Enter Answer"/>
-                            </Form.Group>
-
-                            <Form.Group as={Col}>
-                                <Form.Label>Marks</Form.Label>
-                                <Form.Control name="editLname" type="number" placeholder="Enter Marks"/>
-                            </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Answer: </Form.Label>
+                        <Form.Control name="editFname" type="text" placeholder="Enter Answer"/>
+                    </Form.Group>
+                    <Col sm={1}>
+                        <Form.Group>
+                            <Form.Label>Marks: </Form.Label>
+                            <Form.Control name="editLname" type="number"/>
+                        </Form.Group>
+                    </Col>
+                    <div class="col-auto">
+                        <br/>
+                        <button class="btn btn-primary mb-2">+</button>
+                    </div>
                 </Form.Row>
 
                 <ButtonGroup className="btnGroup">
                     <Button className="btnSubmit" variant="primary" type="submit">
-                        Add New Answer
-                    </Button>
-
-                    <Button className="btnAccessCode ml-5" variant="primary">
                         Create New Question
                     </Button>
                 </ButtonGroup>
